e8c811e20f83275d8671976889572f9480cd0f95
Louis Richardson <102028137+louisrichardson1505@users.noreply.github.com>
2022-10-17 10:49:46 +0100

    test

---
+++ index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+  <link href="style.css" rel="stylesheet">
+</head>
+<body>
+  @include('components.nav-bar')
+  <div class="name-container">
+    <h2 class="name">Louis Richardson</h2>
+    <h2 class="occupation">T-Level: Design, Production, and Development</h2>
+  </div>
+
+</body>
+</html>
